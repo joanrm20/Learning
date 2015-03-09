66656ade47751c8e6cf94a3cb7aa6ab9892b5124
Jose <joseramirez@Joses-MacBook-Pro.local>
2015-03-08 19:26:32 -0600

    Add playground test

--- Languages/Swift/Cat Years/Cat Years/ViewController.swift
+++ Languages/Swift/Cat Years/Cat Years/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var age: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +23,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func findAge(sender: AnyObject) {
+        result.hidden = false
+        var enteredAge = age.text.toInt()
+        
+        if enteredAge != nil{
+            var catYears = enteredAge! * 7
+            
+            result.text = "Your Cat is \(catYears) in cat years"
+        }
+        else {
+            result.text = "Please enter a number in the box"
+        }
+       
+    }
+    
+ 
 
 
 }
---
+++ Languages/Swift/PlayGrounds/MyPlayground.playground/section-1.swift
@@ -0,0 +1,23 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var currentHeartContainers = 5
+let maxHeartContainers = 20
+
+func number() -> Int{
+  return 6
+}
+
+
+number()
+
+// This is a single line comment.
+
+/* This is how you write…
+ALL THE COMMENTS!
+Srsly, all of them. */
+
