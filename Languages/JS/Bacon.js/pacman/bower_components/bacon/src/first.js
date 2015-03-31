0c75192f855a93a4ca69beead76302adfe418126
Jose <joseramirez@Joses-MacBook-Pro.local>
2015-03-30 23:48:16 -0600

    Remove bower componenents

--- Languages/JS/Bacon.js/pacman/bower_components/bacon/src/first.js
+++
@@ -1,6 +0,0 @@
-// build-dependencies: core, take
-
-Bacon.Observable.prototype.first = function () {
-  // It's important not to use fat arrow here!
-  return withDescription(this, "first", this.take(1));
-};
