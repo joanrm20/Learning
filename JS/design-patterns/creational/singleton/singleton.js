bf1fa5c41eea28a6cd08128a593bb1b0fbe18cd3
Jose A. Ramirez <jo.chicho@gmail.com>
2015-04-13 13:54:59 -0600

    Add singleton pattern

--- JS/design-patterns/creational/singleton/singleton.js
+++ JS/design-patterns/creational/singleton/singleton.js
@@ -1,4 +1,33 @@
-var singleton = (function () {
+var singleton = (function() {
   var instance = undefined;
 
-})
+  function init() {
+    //singleton
+
+    var privateVariable = 'This is a private variable';
+    var privateRandomNumber = Math.Random();
+
+    function privateMethod() {
+      console.log('This is a private method')
+    }
+
+    return {
+      publicMethod: function() {
+        console.log('This is public');
+      },
+      publicProperty: "This is also public",
+      getRandomNumber: function() {
+        return privateRandomNumber;
+      }
+    };
+
+    return {
+      getInstance: function() {
+        if (!instance) {
+          instance = init();
+        }
+        return instance;
+      }
+    }
+  }
+})();
