a63a7f42a66c3a9fe7926977f8d872938f57bb6f
Jose A. Ramirez <jo.chicho@gmail.com>
2015-04-17 09:06:19 -0600

    Add way to define object property and properties

--- JS/design-patterns/creational/constructor/object-creation.js
+++ JS/design-patterns/creational/constructor/object-creation.js
@@ -1,4 +1,6 @@
 // Each of the following will create a new empty object
+
+
 // Better to use the literal sintax object creation.
 var newObject = {};
 
@@ -8,3 +10,41 @@ var newObject = Object.create(Object.prototype);
 //or
 
 var newObject = new Object();
+
+// -- Dot syntax
+
+newObject.myKey = "Hey!!";
+
+// Get
+var value = newObject.myKey;
+
+// -- Square brackets syntax
+newObject['myKey'] = "Hi";
+//Get
+var value = newObject['myKey'];
+
+// --Object define properties
+var defineProp = function(obj, key, value) {
+  var config = {
+    value: value,
+    writable: true,
+    enumerable: true,
+    configurable: true
+  };
+  Object.defineProperty(obj, key, config);
+};
+// use
+var person = Object.create(Object.prototype);
+defineProp(person, 'car', 'Delorean');
+
+
+Object.defineProperties(newObject, {
+  "newKey": {
+    value: "hey",
+    writable: true
+  },
+  "anotherNewKey": {
+    value: 'test'
+    writable: false
+  }
+});
