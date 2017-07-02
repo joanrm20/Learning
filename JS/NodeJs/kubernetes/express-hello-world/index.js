1a4000ae274bbe1ccd35fc8c25ba0641ef09d612
Jose Ramirez <jramirez@commonbond.co>
2017-07-01 20:52:52 -0600

    Add kubernetes nodejs express sample

---
+++ JS/NodeJs/kubernetes/express-hello-world/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000...')
+})
